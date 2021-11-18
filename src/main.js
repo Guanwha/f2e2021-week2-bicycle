7db2c6edf7b5bd74ea732c277bec8b35797c1c41
Eric Fei <guanwha@gmail.com>
2021-11-18 11:54:47 +0800

    Loaded custom sass

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import '@/styles/tailwind.css';
+import '@/styles/app.scss';
 
 Vue.config.productionTip = false;
 
