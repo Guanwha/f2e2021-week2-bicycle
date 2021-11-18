24c0f8e1ed8cf88e514ccce731fc13ea27804f5e
Eric Fei <guanwha@gmail.com>
2021-11-19 00:27:35 +0800

    Installed vue2-leaflet 2.7.1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue';
 import VueToast from 'vue-toast-notification';
+import { Icon } from 'leaflet';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import '@/styles/tailwind.css';
 import '@/styles/app.scss';
 import 'vue-toast-notification/dist/theme-default.css';
+import 'leaflet/dist/leaflet.css';
+
+// leaflet
+const iconRetinaUrl = require('leaflet/dist/images/marker-icon-2x.png');
+const iconUrl = require('leaflet/dist/images/marker-icon.png');
+const shadowUrl = require('leaflet/dist/images/marker-shadow.png');
+/* eslint no-underscore-dangle: 0 */
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+});
 
 Vue.use(VueToast);
 Vue.config.productionTip = false;
