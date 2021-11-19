f570ac4440d5cb1842045c26d91e822b7faf7244
Eric Fei <guanwha@gmail.com>
2021-11-20 01:19:02 +0800

    Implemented home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home flex-ccc">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-full h-full flex-ccc bg-main-500">
+    <div class="lex-csbc">
+      <div class="flex-ctl">
+        <img src="@/assets/kt/logo_bike.svg" alt="">
+        <img src="@/assets/kt/Logo.svg" alt="" class="w-60">
+        <div class="text-sm font-bold" style="letter-spacing: 0.92rem">微笑單車．暢遊城市</div>
+      </div>
+      <div class="mt-28 w-full flex-cbc gap-2">
+        <button type="button" class="w-full btn-main border-main p-2">尋找 Youbike</button>
+        <button type="button" class="w-full btn-main border-main p-2">查詢自行車道</button>
+        <button type="button" class="w-full btn-main border-main p-2">附近景點、美食</button>
+      </div>
+    </div>
+    <div class="absolute bottom-5 right-10 text-sm text-second-900">Where’s YouBike  © Code: Crane  /  Design: KT</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
   },
 };
 </script>
